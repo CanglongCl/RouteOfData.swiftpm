8042c69a661e05a51a4520a4d6acd772844dbcf2
Lava <34743145+CanglongCl@users.noreply.github.com>
2024-01-03 00:38:52 -0800

    init commit

---
+++ Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  HelmOfData
+//
+//  Created by 戴藏龙 on 2024/1/3.
+//
+
+import Foundation
+import SwiftData
+
+@available(iOS 17, *)
+@Model
+class MyModel {
+    let hello: String = ""
+
+    init() {
+        hello = ""
+    }
+}
